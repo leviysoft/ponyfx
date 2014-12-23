e11526ad9315f7750cf2b5aaaded2789ac2aefd0
Daniil Smirnov <danslapman@gmail.com>
2014-12-23 23:04:40 +0300

    Partial View[T] implementation

--- src/main/scala/leviysoft/ponyfx/views/View.scala
+++ src/main/scala/leviysoft/ponyfx/views/View.scala
@@ -2,6 +2,29 @@ package leviysoft.ponyfx.views
 
 import javafx.stage.Stage
 
-abstract class View[T] extends Stage{
+import leviysoft.ponyfx.views.DialogResult.DialogResult
 
+import scala.collection.mutable
+
+abstract class View[T] extends Stage {
+  protected var modelChanged: mutable.MutableList[T => Unit] = mutable.MutableList()
+
+  private var vRes = DialogResult.None
+  def viewResult = vRes
+
+  private var modelField: T = throw new IllegalArgumentException()
+  def model = modelField
+  def model_=(value: T) = {
+    modelField = value
+    modelChanged.foreach(f => f(value))
+  }
+
+  protected def endWith(result: DialogResult): Unit = {
+    vRes = result
+    this.close()
+  }
+
+  protected def bind(): Unit = {
+
+  }
 }
